d3f8230bcf139ce794f460e9e4049e9213cb8b72
Christopher <02christopher.villar@gulllakecs.org>
2022-03-07 13:58:31 -0500

    Hobby statement commit and stable

---
+++ CSSE Midterm Review.py
@@ -0,0 +1,3 @@
+#Hobbies
+print(" A hobby that I enjoy is band\n A reason why I like band class is because I have been doing it for a long time.")
+print(" I am also not that creative so it was the only class that I could take to get my arts credit.")
